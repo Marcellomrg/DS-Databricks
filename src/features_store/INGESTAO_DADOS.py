d0ef30fdb66bad246f4fa9b64809155daf5c35cf
Marcellomrg <marcello.usa963@gmail.com>
2025-10-18 01:51:43 +0000

    Feature Store Pontos Criada

--- src/features_store/INGESTAO_DADOS.py
+++ src/features_store/INGESTAO_DADOS.py
@@ -34,7 +34,7 @@ def table_existe(catalog,database,table):
 
 catalog = "feature_store"
 database = "upsell"
-table = "fs_transacoes"
+table = "fs_pontos"
 tableName = f"{catalog}.{database}.{table}"
 
 query = import_query(f"{table}.sql")
